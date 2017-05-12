cc96f6c8c6657def38d5c652d9b611dd74db6ca3
Andrew Zhu <andrewzhu11@gmail.com>
2017-05-12 17:41:21 -0400

    Fixing spacing to 2 spaces

--- src/Direction.java
+++ src/Direction.java
@@ -1,55 +1,55 @@
 /** Defines direction of movement, and transformations for symmetries */
 public class Direction {
-   private int dX;
-   private int dY;
+  private int dX;
+  private int dY;
 
-   public Direction() {
-      this.dX = 1;
-      this.dY = 0;
-   }
+  public Direction() {
+    this.dX = 1;
+    this.dY = 0;
+  }
 
-   public Direction(int dX, int dY) {
-      if (dX == 0 && dY == 0)
-        dX = 1;
-      this.dX = dX;
-      this.dY = dY;
-   }
+  public Direction(int dX, int dY) {
+    if (dX == 0 && dY == 0)
+      dX = 1;
+    this.dX = dX;
+    this.dY = dY;
+  }
 
-   public Direction(Direction other) {
-      this.dX = other.dX;
-      this.dY = other.dY;
-   }
+  public Direction(Direction other) {
+    this.dX = other.dX;
+    this.dY = other.dY;
+  }
 
-   public boolean isForward() {
-      return dY > 0;
-   }
+  public boolean isForward() {
+    return dY > 0;
+  }
 
-   public boolean isRight() {
-      return dX > 0;
-   }
+  public boolean isRight() {
+    return dX > 0;
+  }
 
-   public Direction forwSymm() {
-      return new Direction(dX, -1 * dY);
-   }
+  public Direction forwSymm() {
+    return new Direction(dX, -1 * dY);
+  }
 
-   public Direction sideSymm() {
-      return new Direction(-1 * dX, dY);
-   }
+  public Direction sideSymm() {
+    return new Direction(-1 * dX, dY);
+  }
 
-   public Direction diagSymm() {
-      return new Direction(dY, dX);
-   }
+  public Direction diagSymm() {
+    return new Direction(dY, dX);
+  }
 
-   public int xDist() {
-      return dX;
-   }
+  public int xDist() {
+    return dX;
+  }
 
-   public int yDist() {
-      return dY;
-   }
+  public int yDist() {
+    return dY;
+  }
 
-   @Override
-   public String toString() {
-      return "(" + Integer.toString(dX) + ", " + Integer.toString(dY) + ")";
-   }
+  @Override
+  public String toString() {
+    return "(" + Integer.toString(dX) + ", " + Integer.toString(dY) + ")";
+  }
 }
