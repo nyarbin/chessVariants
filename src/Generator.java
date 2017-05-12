31485c3744cf26fda53c2f3dad11ea694e410d02
Ben Reis <unbeliever536@gmail.com>
2017-05-12 17:21:01 -0400

    started work on GDL output

--- src/Generator.java
+++ src/Generator.java
@@ -23,4 +23,68 @@ class Generator {
 
     /* do genetic algorithm rounds */
   }
+
+  /** Basic info for GDL */
+  private String headerInfo() {
+     return "(role white)\n(role black)\n(init (control white)\n)";
+  }
+
+  /** GDL sentences describing initial board state */
+  private String initialization() {
+  }
+
+  /** The two basic move rules: noop off-turn; move on-turn. */
+  private String baseRules() {
+     String toReturn;
+     toReturn = "(<= (legal ?player ?noop)\n (role ?player)\n" +
+       " (not (true (control ?player))))\n";
+     toReturn += "(<= (legal ?player ?move)\n (true (control ?player))\n" +
+        " (legalMove ?player ?move))\n";
+     return toReturn;
+  }
+
+  /** GDL sentences describing how a given piece moves */
+  private String pieceMovement(Piece piece) {
+     //TODO (needs addition)
+     String toReturn;
+
+     for (Move : piece.moves) {
+     }
+
+     return toReturn;
+  }
+
+  /** Base/input clauses */
+  private String base() {
+  }
+
+  /** GDL definitions for fundamental constants and math */
+  private String constants(int files, int ranks) {
+     String toReturn;
+
+     toReturn = "(opponent white black)\n(opponent black white)\n";
+     for (int i = 0; i < files; i++) {
+        toReturn += "(file " + toString(i) + ")\n";
+     }
+     for (int i = 0; i < ranks; i++) {
+        toReturn += "(rank " + toString(i) + ")\n";
+     }
+
+     //Less-than relation
+     toReturn += "(<= (lt ?x ?y) (succ ?x ?y))\n" +
+        "(<= (lt ?x ?z) (succ ?x ?y) (lt ?y ?z))\n";
+
+     //Addition
+     toReturn += "(<= (add ?x 0 ?x)\n (number ?x)\n";
+     toReturn += "(<= (add ?x ?y ?z)\n (succ ?x ?x1)\n (succ ?y1 ?y)\n" +
+        " (add ?x1 ?y1 ?z))\n";
+     toReturn += "(number 0)\n";
+     toReturn += "(<= (number ?y) (succ ?x ?y))\n"
+
+     //Define the first 500 numbers into existence
+     for (int i = 0; i < 500; i++) {
+        toReturn += "(succ " + toString(i) + " " toString(i + 1) + ")\n";
+     return toReturn;
+  }
+
 }
