69f7d6409b77055c3b5e90a11c335f78a0bcc7c5
Andrew Zhu <andrewzhu11@gmail.com>
2017-05-12 17:00:52 -0400

    Minor updates to Generator variable names

--- src/Generator.java
+++ src/Generator.java
@@ -4,14 +4,14 @@ import java.util.Random;
 
 /** Main generator class for chess variants */
 class Generator {
-  public static final int BOARD_SIZE = 5;       // size of board (n by n grid)
-  public static final int PLACEMENT_ROWS = 1;   // # of ranks pieces can be placed in
-  public static final int MAX_PIECES = 5;       // max pieces per side (including king)
-  public static final int MAX_SCORE = 50;       // max score of pieces (excluding king)
+  public static final int BOARD_SIZE = 5;     // size of board (n by n grid)
+  public static final int PLACEMENT_ROWS = 1; // # ranks player places pieces in
+  public static final int MAX_PIECES = 5;     // max pieces per side (w/o king)
+  public static final int MAX_SCORE = 50;     // max score of pieces (w/ king)
 
   private static final int NUM_ROUNDS = 100;
-  private static final int NUM_PIECE_TYPES = 4; // number of pieces available to use
-  private static List<Piece> _pieces;           // available pieces in current round
+  private static final int INIT_PIECES = 4;   // number of pieces initially
+  private static List<Piece> _pieces;         // piece list for current round
   private static Random _random;
 
   /** Runs genetic algorithm to pick chess variants */
@@ -19,7 +19,7 @@ class Generator {
     _random = new Random();
     _pieces = new ArrayList<Piece>();
     /* Initialize pieces */
-    for (int i = 0; i < NUM_PIECE_TYPES; i++)
+    for (int i = 0; i < INIT_PIECES; i++)
       _pieces.add(new Piece(_random));
     /* do genetic algorithm rounds */
     for (int r = 0; r < NUM_ROUNDS; r++) {
